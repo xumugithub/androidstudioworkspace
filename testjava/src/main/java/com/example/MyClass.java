616b91a2acd00fab63cb6e926a35ab4087e7656c
xumugithub <1750696382@qq.com>
2017-04-07 23:21:02 +0800

    第一次提交

---
+++ testjava/src/main/java/com/example/MyClass.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MyClass {
+    public static void main(String[] args) {
+        /*File file = new File("D:/1111.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
